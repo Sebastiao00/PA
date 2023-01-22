c38f5a87b04a5014fd61dc265cc9ef46ed0d679e
Infinty_free <98946854+Sebastiao00@users.noreply.github.com>
2023-01-22 16:41:32 +0000

    Add files via upload

--- Pape/profile/profile.php
+++ Pape/profile/profile.php
@@ -13,12 +13,12 @@
         <link href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css" />
         <!-- Core theme CSS (includes Bootstrap)-->
-        <link href="css/styles.css" rel="stylesheet" />
+        <link href="C:\xampp\htdocs\Pape\css\profile.css" rel="stylesheet" />
     </head>
     <body>
         <!-- Navigation-->
         <?php
-            include "assets/includes/navbar.php";
+            include "../assets/includes/navbar.php";
         ?>	
         <div class="container rounded bg-white mt-5 mb-5">
             <div class="row">
